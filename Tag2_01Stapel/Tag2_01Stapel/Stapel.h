f8af64dbf92d8564e210a12947c5cf9c3fa4dd58
Jo Wagner <jowagner@limago.de>
2022-06-28 10:04:32 +0200

    Uebung

--- Tag2_01Stapel/Tag2_01Stapel/Stapel.h
+++ Tag2_01Stapel/Tag2_01Stapel/Stapel.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <iostream>
+
 namespace vw
 {
 
@@ -7,29 +10,47 @@ namespace vw
 	{
 
 
-
+		/// <summary>
+		/// Fachliche Beschreibung (Dokumentation)
+		/// </summary>
 		class Stapel
 		{
 		public:
-			Stapel(){}
-
-			void push(const int value)
+			/// <summary>
+			/// 
+			/// </summary>
+			Stapel()
 			{
-				
+				std::cout << "ctor" << std::endl;
 			}
 
-			int pop()
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="value"></param>
+			void push(const int value) // Verhalten im Fehlerfall
 			{
+				std::cout << "push" << std::endl;
+			}
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <returns></returns>
+			int pop() // Verhalten im Fehlerfall
+			{
+				std::cout << "pop" << std::endl;
 				return 0;
 			}
 
 			bool is_empty()
 			{
+				std::cout << "is_empty" << std::endl;
 				return true;
 			}
 
 			bool is_full()
 			{
+				std::cout << "is_full" << std::endl;
 				return true;
 			}
 		};
--- Tag2_01Stapel/Tag2_01Stapel/Tag2_01Stapel.cpp
+++ Tag2_01Stapel/Tag2_01Stapel/Tag2_01Stapel.cpp
@@ -2,10 +2,22 @@
 //
 
 #include <iostream>
+#include "Stapel.h"
+
+using namespace vw::collections;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Stapel myStapel;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        if (!myStapel.is_full())
+            myStapel.push(i);
+    }
+
+    while (!myStapel.is_empty())
+        std::cout << myStapel.pop() << std::endl;
 }
 
 
